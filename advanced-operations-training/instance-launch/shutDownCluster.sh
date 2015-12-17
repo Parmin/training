970cd65e3b968cba8966b4f37416038724d007be
shannonbradshaw <shannon.bradshaw@gmail.com>
2015-12-17 10:18:54 -0500

    Changed name to advanced-administrators-training

--- advanced-operations-training/instance-launch/shutDownCluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-aws ec2 --profile wargaming terminate-instances --instance-ids `cat sharded_instances`
-rm sharded_instances
--- advanced-operations-training/instance-launch/shutDownRepl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for id in $(cat repl_instances)
-do
-    aws ec2 --profile wargaming terminate-instances --instance-ids "$id"
-done
-rm repl_instances
