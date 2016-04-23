7a19ef0f29ca6dd57b34b94d8f109c332e462f10
William Cross <will@Williams-MacBook-Pro.local>
2016-04-23 14:34:24 -0400

    Finished a lesson for the new CRUD API

--- source/includes/crud-updating-documents.js
+++ source/includes/crud-updating-documents.js
@@ -6,12 +6,13 @@ db.movies.insertOne( { title: "Batman" } )
 db.movies.find()
 db.movies.replaceOne( { title : "Batman" }, { imdb_rating : 7.7 } )
 db.movies.find()
-db.movies.replaceOne( { imdb_rating: 7.7 }, { title: "Batman", imdb_rating: 7.7 } )    
+db.movies.replaceOne( { imdb_rating: 7.7 },
+                      { title: "Batman", imdb_rating: 7.7 } )    
 db.movies.find()
 db.movies.replaceOne( { }, { title: "Batman" } )
 db.movies.find()  // back in original state
 db.movies.replaceOne( { }, { _id : ObjectId() } )
-
+db.movies.drop()
 
 
 // $set and unset
@@ -34,25 +35,30 @@ db.movies.insertMany( [
     "imdb_rating" : 7.4 
   }
 ] )
-db.movies.updateOne( { "title" : "Batman" }, { $set : { "imdb_rating" : 7.7 } } )  
-db.movies.updateOne( { "title" : "Godzilla" }, { $set : { "budget" : 1 } } ) 
+db.movies.updateOne( { "title" : "Batman" },
+                     { $set : { "imdb_rating" : 7.7 } } )  
+db.movies.updateOne( { "title" : "Godzilla" },
+                     { $set : { "budget" : 1 } } ) 
 db.movies.updateOne( { "title" : "Home Alone" }, 
-                     { $set : { "budget" : 15, "imdb_rating" : 5.5 } } )
-db.movies.updateOne( { "title" : "Home Alone" }, { $unset :  { "budget" : 1 } } )
-
+                     { $set : { "budget" : 15,
+                                "imdb_rating" : 5.5 } } )
+db.movies.updateOne( { "title" : "Home Alone" },
+                     { $unset :  { "budget" : 1 } } )
+db.movies.find()
 
 // update operators
-db.movies.updateOne( { title: "Batman" }, { $inc: { "imdb_rating" : 2 } } )        
-db.movies.updateOne( { title: "Home Alone" }, { $inc: { "budget" : 5 } } )  
-db.movies.updateOne( { title: "Batman" }, { $mul: { "imdb_rating" : 4 } } )  
-db.movies.updateOne( { title: "Batman" }, 
+db.movies.updateOne( { title: "Batman" }, { $inc: { "imdb_rating" : 2 } } )
+db.movies.updateOne( { title: "Home Alone" }, { $inc: { "budget" : 5 } } )
+db.movies.updateOne( { title: "Batman" }, { $mul: { "imdb_rating" : 4 } } )
+db.movies.updateOne( { title: "Batman" },
                      { $rename: { budget: "estimated_budget" } } )
-db.movies.updateOne( { title: "Home Alone" }, { $min: { budget: 5 } } )  
-db.movies.updateOne( { title: "Home Alone" }, 
-                     { $currentDate : { last_updated: { $type: "timestamp" } } } )
+db.movies.updateOne( { title: "Home Alone" }, { $min: { budget: 5 } } )
+db.movies.updateOne( { title: "Home Alone" },
+      { $currentDate : { last_updated: { $type: "timestamp" } } } )
 // increment movie mentions by 10
-db.movie_mentions.updateOne( { title: "E.T." } , 
-                             { $inc:  { "mentions_per_hour.5" : 10 } } )                               
+db.movie_mentions.updateOne( { title: "E.T." },
+      { $inc:  { "mentions_per_hour.5" : 10 } } )                               
+
 
 
 // updateMany vs. updateOne
@@ -62,4 +68,175 @@ db.movies.find()
 // we need updateMany to change all documents 
 db.movies.updateMany( { }, { $set : { "sequels" : 0 } } )
 db.movies.find()
+
+
+
+
+
+
+
+// Example: Update Array Elements by Index
+// add a sample document to track mentions per hour
+db.movie_mentions.insertOne(
+    { "title" : "E.T.", 
+      "day" : ISODate("2015-03-27T00:00:00.000Z"), 
+      "mentions_per_hour" : [ 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+      0, 0 ] 
+    } )
+
+// update all mentions for the fifth hour of the day
+db.movie_mentions.updateOne(
+    { "title" : "E.T." } , 
+    { $set :  { "mentions_per_hour.5" : 2300 } } )                               
+
+
+
+
+
+
+// Example: Array Operators
+db.movies.updateOne(
+    { "title" : "Batman" }, 
+    { $push : { "category" : "superhero" } } ) 
+db.movies.updateOne(
+    { "title" : "Batman" }, 
+    { $pushAll : { "category" : [ "villain", "comic-based" ] } } ) 
+db.movies.updateOne(
+    { "title" : "Batman" }, 
+    { $pop : { "category" : 1 } } )  
+db.movies.updateOne(
+    { "title" : "Batman" }, 
+    { $pull : { "category" : "action" } } )  
+db.movies.updateOne(
+    { "title" : "Batman" }, 
+    { $pullAll : { "category" : [ "villain", "comic-based" ] } } ) 
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $addToSet : { "category" : "action" } } )  
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $addToSet : { "category" : "action" } } )  
+// The Positional ``$`` Operator
+     db.<COLLECTION>.updateOne(
+         { <array> : value ... },
+         { <update operator> : { "<array>.$" : value } }
+     )
+
+
+
+
+
+
+
+// Example: The Positional ``$`` Operator
+// the "action" category needs to be changed to "action-adventure"
+db.movies.updateOne( { "category": "action",  }, 
+                     { $set: { "category.$" : "action-adventure" } }, 
+                     { multi: true } )
+db.movies.find()
+
+
+
+
+
+
+
+
+
+
+// Upserts
+db.<COLLECTION>.updateOne( <query document>,
+                           <update document>, 
+                           { upsert: true } )
+
+
+
+
+
+
+
+
+
+
+// Example: Upserts
+db.movies.updateOne( { "title" : "Jaws" }, 
+                     { $inc: { "budget" : 5 } }, 
+                     { upsert: true } )
+
+db.movies.updateMany( { "title" : "Jaws II" }, 
+                      { $inc: { "budget" : 5 } }, 
+                      { upsert: true } )
+
+db.movies.replaceOne( { "title" : "Jaws III", "category" : [ "horror" ] }, 
+                      { $set : { "budget" : 1 } }, 
+                      { upsert: true } )
+
+
+
+
+
+
+
+
+
+// ``save()``
+db.<COLLECTION>.save( <document> )
+
+
+
+
+
+
+
+
+
+
+// Example: ``save()``
+// insert
+db.movies.save( { "title" : "Beverly Hills Cops", "imdb_rating" : 7.3 })            
+
+// update with { upsert: true }
+db.movies.save( { "_id" : 1234, "title" : "Spider Man", "imdb_rating" : 7.3 })  
+
+
+
+
+
+
+
+
+
+// Be Careful with ``save()``
+db.movies.drop()
+db.movies.insertOne( { "title" : "Jaws", "imdb_rating" : 7.3 } )
+
+db.movies.find( { "title" : "Jaws" } )
+
+// store the complete document in the application
+doc = db.movies.findOne( { "title" : "Jaws" } )
+
+db.movies.updateOne( { "title" : "Jaws"  }, { $inc: { "imdb_rating" : 2 } } )
+db.movies.find()
+
+doc.imdb_rating = 7.4
+doc
+
+db.movies.save(doc)  // just lost our incrementing of "imdb_rating"
+db.movies.find()
+
+
+
+
+
+
+
+
+// findOneAndReplace Example
+db.worker_queue.findOneAndUpdate(
+    { state : "unprocessed" },
+    { $set: { "worker_id" : 123, "state" : "processing" } },
+    { upsert: true } )
+
+
 
