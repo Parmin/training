45ee818e59973febe680c79b2b26dd0df55c8c82
shannonbradshaw <shannon.bradshaw@gmail.com>
2016-05-20 11:58:42 -0400

    More CRUD cleanup

--- source/includes/crud-updating-documents.js
+++ source/includes/crud-updating-documents.js
@@ -7,39 +7,39 @@ db.movies.find()
 db.movies.replaceOne( { title : "Batman" }, { imdb_rating : 7.7 } )
 db.movies.find()
 db.movies.replaceOne( { imdb_rating: 7.7 },
-                      { title: "Batman", imdb_rating: 7.7 } )    
+                      { title: "Batman", imdb_rating: 7.7 } )
 db.movies.find()
 db.movies.replaceOne( { }, { title: "Batman" } )
 db.movies.find()  // back in original state
 db.movies.replaceOne( { }, { _id : ObjectId() } )
-db.movies.drop()
+
 
 
 // $set and unset
-db.movies.insertMany( [ 
-  { 
-    "title" : "Batman", 
-    "category" : [ "action", "adventure" ], 
-    "imdb_rating" : 7.6, 
-    "budget" : 35 
+db.movies.insertMany( [
+  {
+    "title" : "Batman",
+    "category" : [ "action", "adventure" ],
+    "imdb_rating" : 7.6,
+    "budget" : 35
   },
-  { 
-    "title" : "Godzilla", 
-    "category" : [ "action", 
-    "adventure", "sci-fi" ], 
-    "imdb_rating" : 6.6 
+  {
+    "title" : "Godzilla",
+    "category" : [ "action",
+    "adventure", "sci-fi" ],
+    "imdb_rating" : 6.6
   },
-  { 
-    "title" : "Home Alone", 
-    "category" : [ "family", "comedy" ], 
-    "imdb_rating" : 7.4 
+  {
+    "title" : "Home Alone",
+    "category" : [ "family", "comedy" ],
+    "imdb_rating" : 7.4
   }
 ] )
 db.movies.updateOne( { "title" : "Batman" },
-                     { $set : { "imdb_rating" : 7.7 } } )  
+                     { $set : { "imdb_rating" : 7.7 } } )
 db.movies.updateOne( { "title" : "Godzilla" },
-                     { $set : { "budget" : 1 } } ) 
-db.movies.updateOne( { "title" : "Home Alone" }, 
+                     { $set : { "budget" : 1 } } )
+db.movies.updateOne( { "title" : "Home Alone" },
                      { $set : { "budget" : 15,
                                 "imdb_rating" : 5.5 } } )
 db.movies.updateOne( { "title" : "Home Alone" },
@@ -57,7 +57,7 @@ db.movies.updateOne( { title: "Home Alone" },
       { $currentDate : { last_updated: { $type: "timestamp" } } } )
 // increment movie mentions by 10
 db.movie_mentions.updateOne( { title: "E.T." },
-      { $inc:  { "mentions_per_hour.5" : 10 } } )                               
+      { $inc:  { "mentions_per_hour.5" : 10 } } )
 
 
 
@@ -65,7 +65,7 @@ db.movie_mentions.updateOne( { title: "E.T." },
 // let's start tracking the number of sequals for each movie
 db.movies.updateOne( { }, { $set : { "sequels" : 0 } } )
 db.movies.find()
-// we need updateMany to change all documents 
+// we need updateMany to change all documents
 db.movies.updateMany( { }, { $set : { "sequels" : 0 } } )
 db.movies.find()
 
@@ -78,17 +78,17 @@ db.movies.find()
 // Example: Update Array Elements by Index
 // add a sample document to track mentions per hour
 db.movie_mentions.insertOne(
-    { "title" : "E.T.", 
-      "day" : ISODate("2015-03-27T00:00:00.000Z"), 
+    { "title" : "E.T.",
+      "day" : ISODate("2015-03-27T00:00:00.000Z"),
       "mentions_per_hour" : [ 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-      0, 0 ] 
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0 ]
     } )
 
 // update all mentions for the fifth hour of the day
 db.movie_mentions.updateOne(
-    { "title" : "E.T." } , 
-    { $set :  { "mentions_per_hour.5" : 2300 } } )                               
+    { "title" : "E.T." } ,
+    { $set :  { "mentions_per_hour.5" : 2300 } } )
 
 
 
@@ -97,26 +97,26 @@ db.movie_mentions.updateOne(
 
 // Example: Array Operators
 db.movies.updateOne(
-    { "title" : "Batman" }, 
-    { $push : { "category" : "superhero" } } ) 
-db.movies.updateOne(
-    { "title" : "Batman" }, 
-    { $pushAll : { "category" : [ "villain", "comic-based" ] } } ) 
-db.movies.updateOne(
-    { "title" : "Batman" }, 
-    { $pop : { "category" : 1 } } )  
-db.movies.updateOne(
-    { "title" : "Batman" }, 
-    { $pull : { "category" : "action" } } )  
-db.movies.updateOne(
-    { "title" : "Batman" }, 
-    { $pullAll : { "category" : [ "villain", "comic-based" ] } } ) 
+    { "title" : "Batman" },
+    { $push : { "category" : "superhero" } } )
 db.movies.updateOne(
     { "title" : "Batman" },
-    { $addToSet : { "category" : "action" } } )  
+    { $pushAll : { "category" : [ "villain", "comic-based" ] } } )
 db.movies.updateOne(
     { "title" : "Batman" },
-    { $addToSet : { "category" : "action" } } )  
+    { $pop : { "category" : 1 } } )
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $pull : { "category" : "action" } } )
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $pullAll : { "category" : [ "villain", "comic-based" ] } } )
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $addToSet : { "category" : "action" } } )
+db.movies.updateOne(
+    { "title" : "Batman" },
+    { $addToSet : { "category" : "action" } } )
 // The Positional ``$`` Operator
      db.<COLLECTION>.updateOne(
          { <array> : value ... },
@@ -131,9 +131,9 @@ db.movies.updateOne(
 
 // Example: The Positional ``$`` Operator
 // the "action" category needs to be changed to "action-adventure"
-db.movies.updateOne( { "category": "action",  }, 
-                     { $set: { "category.$" : "action-adventure" } }, 
-                     { multi: true } )
+db.movies.updateMany( { "category": "action",  },
+                      { $set: { "category.$" : "action-adventure" } } )
+
 db.movies.find()
 
 
@@ -147,7 +147,7 @@ db.movies.find()
 
 // Upserts
 db.<COLLECTION>.updateOne( <query document>,
-                           <update document>, 
+                           <update document>,
                            { upsert: true } )
 
 
@@ -160,16 +160,16 @@ db.<COLLECTION>.updateOne( <query document>,
 
 
 // Example: Upserts
-db.movies.updateOne( { "title" : "Jaws" }, 
-                     { $inc: { "budget" : 5 } }, 
+db.movies.updateOne( { "title" : "Jaws" },
+                     { $inc: { "budget" : 5 } },
                      { upsert: true } )
 
-db.movies.updateMany( { "title" : "Jaws II" }, 
-                      { $inc: { "budget" : 5 } }, 
+db.movies.updateMany( { "title" : "Jaws II" },
+                      { $inc: { "budget" : 5 } },
                       { upsert: true } )
 
-db.movies.replaceOne( { "title" : "Jaws III", "category" : [ "horror" ] }, 
-                      { $set : { "budget" : 1 } }, 
+db.movies.replaceOne( { "title" : "Jaws III", "category" : [ "horror" ] },
+                      { $set : { "budget" : 1 } },
                       { upsert: true } )
 
 
@@ -194,10 +194,10 @@ db.<COLLECTION>.save( <document> )
 
 // Example: ``save()``
 // insert
-db.movies.save( { "title" : "Beverly Hills Cops", "imdb_rating" : 7.3 })            
+db.movies.save( { "title" : "Beverly Hills Cops", "imdb_rating" : 7.3 })
 
 // update with { upsert: true }
-db.movies.save( { "_id" : 1234, "title" : "Spider Man", "imdb_rating" : 7.3 })  
+db.movies.save( { "_id" : 1234, "title" : "Spider Man", "imdb_rating" : 7.3 })
 
 
 
@@ -220,7 +220,7 @@ db.movies.updateOne( { "title" : "Jaws"  }, { $inc: { "imdb_rating" : 2 } } )
 db.movies.find()
 
 doc.imdb_rating = 7.4
-doc
+
 
 db.movies.save(doc)  // just lost our incrementing of "imdb_rating"
 db.movies.find()
@@ -248,13 +248,3 @@ db.foo.insertMany( [ { a : 1 }, { a : 2 }, { a : 3 } ] );
 db.foo.find();  // shows the documents.
 db.foo.findOneAndDelete( { a : { $lte : 3 } } );
 db.foo.find();
-
-
-
-    
-
-
-
-
-
-
