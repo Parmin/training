deb0f5da5636272f556a8e8d350872a31c1cad69
Norberto <norberto@10gen.com>
2016-06-25 14:02:22 -0400

    EDU-3198 - adding placeholders for services

--- mongomart/microservices/services/search.py
+++ mongomart/microservices/services/search.py
@@ -1,8 +1,14 @@
 import bottle
+import pymongo
 
+@get('/search')
+def search_items(query):
+    pass
 
 
 
 bottle.debug(True)
 # Start the webserver running and wait for requests
-bottle.run(host='localhost', port=8080)
+bottle.run(host='localhost', port=9080)
+
+db = {}
