bd3357babcf3fff6fdbf01ba973b7b5bda2404c8
Norberto <norberto@10gen.com>
2016-06-25 21:06:44 -0400

    EDU-3198 - adding reviews service

--- mongomart/microservices/services/search.py
+++ mongomart/microservices/services/search.py
@@ -1,14 +1,15 @@
 import bottle
 import pymongo
 
+db = {}
+
 @get('/search')
 def search_items(query):
     pass
 
 
 
+
 bottle.debug(True)
 # Start the webserver running and wait for requests
 bottle.run(host='localhost', port=9080)
-
-db = {}
