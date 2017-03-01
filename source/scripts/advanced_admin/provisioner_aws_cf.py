ac3fb404074d55d2d0044f5020e0a5e7e427270a
Daniel Coupal <dcoupal@Daniels-MacBook-Pro.local>
2017-02-28 17:14:05 -0800

    Adding the creation of a separate JSON file per team

--- source/scripts/advanced_admin/provisioner_aws_cf.py
+++ source/scripts/advanced_admin/provisioner_aws_cf.py
@@ -120,13 +120,19 @@ class Provisioner_aws_cf(object):
                     more_results = False
         else:
             # TODO - remove all the files that would match this run, and for all teams
-            run_info = self.get_run_info(printit=True, perteam=True)
+            (run_info, teams_info) = self.get_run_info(printit=True, perteam=True)
             if self.args.out:
                 try:
                     f = open(self.args.out, 'w')
                     contents = json.dumps(run_info, indent=2)
                     f.write(contents + "\n")
                     f.close()
+                    for key in teams_info.keys():
+                        team_info = teams_info[key].get_dict()
+                        f = open(self.args.out + "-" + key, 'w')
+                        contents = json.dumps(team_info, indent=2)
+                        f.write(contents + "\n")
+                        f.close()
                 except Exception, e:
                     fatal(1, e.__str__())
                 print("\nJSON file for the above run has been saved in: {}".format(self.args.out))
@@ -142,54 +148,72 @@ class Provisioner_aws_cf(object):
 
     def get_run_info(self, printit, perteam=False):
         run_info = BuildAndPrintDict(printit)
+        teams_info = dict()
 
+        # Can't describe a run in 'CREATE_IN_PROGRESS'
         run_description = self._describe_stack(self.training_run)
-        run_resources = self._describe_stack_resources(self.training_run)
-        runinfo = self._get_outputs_for_stack(self.training_run)
-        run_info.comment("")
-        run_info.key_values(("Name", self.training_run))
-        run_info.key_values(("KeyPair", runinfo['KeyPair']))
-        run_info.key_values(("NumberOfTeams", runinfo['NbTeams']))
-        run_info.comment("")
-        run_info.start_list("Teams")
-        # Describe all sub stacks, starting with the top one for the 'run'
-        for one_run_resource in run_resources['StackResources']:
-            # Need VPC, key/pair,
-            if one_run_resource['ResourceType'] == 'AWS::CloudFormation::Stack':
-                # This is one team
-                physicalId = one_run_resource['PhysicalResourceId']
-                teaminfo = self._get_outputs_for_stack(physicalId)
-                run_info.comment("")
-                run_info.new_list_obj()
-                run_info.key_values(("Id", teaminfo['TeamNumber']))
-                run_info.key_values(("LoadBalancer", teaminfo['LoadBalancerHostName']))
-                run_info.key_values(("SecurityGroup", teaminfo['SSHandHTTPSecurityGroup']))
-                run_info.key_values(("VPC", teaminfo['VPC']))
-                run_info.key_values(("SubnetMask", teaminfo['SubnetMask']))
-                # Getting all resources for the stack that created this team
-                team_resources = self._describe_stack_resources(physicalId)
-                run_info.start_list("Hosts")
-                for one_team_resource in team_resources['StackResources']:
-                    # Need Instances, Load Balancer, subnet, team number
-                    if one_team_resource['ResourceType'] == 'AWS::CloudFormation::Stack':
-                        # Need Instance ID, IP, Role
-                        run_info.new_list_obj()
-                        physicalId = one_team_resource['PhysicalResourceId']
-                        hostinfo = self._get_outputs_for_stack(physicalId)
-                        hosttags = self._get_tags_for_stack(physicalId)
-                        run_info.key_values( ("Id", hostinfo['InstanceID'], "{:10}"), ("Role", hosttags['Role'], "{:8}") )
-                        # If the machine rebooted, it would have a new public name and public IP
-                        #run_info.key_values( ("PrivateIP", hostinfo['PrivateIP'], "{:15}"), ("PrivateName", hostinfo['PrivateDnsName'], "{:50}") )
-                        #run_info.key_values( ("PublicIP ", hostinfo['PublicIP'], "{:15}"), ("PublicName ", hostinfo['PublicDnsName'], "{:50}") )
-                        # ... so get the current info by querying the instance info directly
-                        instanceinfo = self._get_instance_info(hostinfo['InstanceID'])
-                        run_info.key_values( ("PrivateIP", instanceinfo['PrivateIP'], "{:15}"), ("PrivateName", instanceinfo['PrivateDnsName'], "{:50}") )
-                        run_info.key_values( ("PublicIP", instanceinfo['PublicIP'], "{:15}"), ("PublicName", instanceinfo['PublicDnsName'], "{:50}") )
-                        run_info.comment("")
-                run_info.end_list("Hosts")
-        run_info.end_list("Teams")
-        run_info.comment("")
-        return run_info.get_dict()
+        if run_description['StackStatus'] == 'CREATE_IN_PROGRESS':
+            print("\nERROR - Can't describe a stack in 'CREATE_IN_PROGRESS' state")
+        else:
+            run_resources = self._describe_stack_resources(self.training_run)
+            runinfo = self._get_outputs_for_stack(self.training_run)
+            run_info.comment("")
+            run_info.key_values(("Name", self.training_run))
+            run_info.key_values(("KeyPair", runinfo['KeyPair']))
+            run_info.key_values(("NumberOfTeams", runinfo['NbTeams']))
+            run_info.comment("")
+            run_info.start_list("Teams")
+            # Describe all sub stacks, starting with the top one for the 'run'
+            for one_run_resource in run_resources['StackResources']:
+                # Need VPC, key/pair,
+                if one_run_resource['ResourceType'] == 'AWS::CloudFormation::Stack':
+                    # This is one team
+                    one_team_info = BuildAndPrintDict(printit=False)
+                    physicalId = one_run_resource['PhysicalResourceId']
+                    teaminfo = self._get_outputs_for_stack(physicalId)
+                    run_info.comment("")
+                    run_info.new_list_obj()
+                    run_info.key_values(("Id", teaminfo['TeamNumber']))
+                    run_info.key_values(("LoadBalancer", teaminfo['LoadBalancerHostName']))
+                    run_info.key_values(("SecurityGroup", teaminfo['SSHandHTTPSecurityGroup']))
+                    run_info.key_values(("VPC", teaminfo['VPC']))
+                    run_info.key_values(("SubnetMask", teaminfo['SubnetMask']))
+                    # Keep also the info for the team
+                    one_team_info.key_values(("Id", teaminfo['TeamNumber']))
+                    one_team_info.key_values(("LoadBalancer", teaminfo['LoadBalancerHostName']))
+                    one_team_info.key_values(("SecurityGroup", teaminfo['SSHandHTTPSecurityGroup']))
+                    one_team_info.key_values(("VPC", teaminfo['VPC']))
+                    one_team_info.key_values(("SubnetMask", teaminfo['SubnetMask']))
+                    # Getting all resources for the stack that created this team
+                    team_resources = self._describe_stack_resources(physicalId)
+                    run_info.start_list("Hosts")
+                    one_team_info.start_list("Hosts")
+                    for one_team_resource in team_resources['StackResources']:
+                        # Need Instances, Load Balancer, subnet, team number
+                        if one_team_resource['ResourceType'] == 'AWS::CloudFormation::Stack':
+                            # Need Instance ID, IP, Role
+                            run_info.new_list_obj()
+                            physicalId = one_team_resource['PhysicalResourceId']
+                            hostinfo = self._get_outputs_for_stack(physicalId)
+                            hosttags = self._get_tags_for_stack(physicalId)
+                            run_info.key_values( ("Id", hostinfo['InstanceID'], "{:10}"), ("Role", hosttags['Role'], "{:8}") )
+                            # If the machine rebooted, it would have a new public name and public IP
+                            # ... so get the current info by querying the instance info directly
+                            instanceinfo = self._get_instance_info(hostinfo['InstanceID'])
+                            run_info.key_values( ("PrivateIP", instanceinfo['PrivateIP'], "{:15}"), ("PrivateName", instanceinfo['PrivateDnsName'], "{:50}") )
+                            run_info.key_values( ("PublicIP", instanceinfo['PublicIP'], "{:15}"), ("PublicName", instanceinfo['PublicDnsName'], "{:50}") )
+                            run_info.comment("")
+                            # Doing the same for the team
+                            one_team_info.new_list_obj()
+                            one_team_info.key_values( ("Id", hostinfo['InstanceID'], "{:10}"), ("Role", hosttags['Role'], "{:8}") )
+                            one_team_info.key_values( ("PrivateIP", instanceinfo['PrivateIP'], "{:15}"), ("PrivateName", instanceinfo['PrivateDnsName'], "{:50}") )
+                            one_team_info.key_values( ("PublicIP", instanceinfo['PublicIP'], "{:15}"), ("PublicName", instanceinfo['PublicDnsName'], "{:50}") )
+                    run_info.end_list("Hosts")
+                    one_team_info.end_list("Hosts")
+                    teams_info[teaminfo['TeamNumber']] = one_team_info
+            run_info.end_list("Teams")
+            run_info.comment("")
+        return(run_info.get_dict(), teams_info)
 
     def manage(self, cmd, script):
         """
@@ -197,7 +221,7 @@ class Provisioner_aws_cf(object):
         """
         etchosts_filename = "/tmp/hosts"
         # Get the list of hosts from the deployment
-        run_info = self.get_run_info(printit=False, perteam=False)
+        (run_info, _) = self.get_run_info(printit=False, perteam=False)
         keypair = run_info['KeyPair']
         for team in run_info['Teams']:
             if (self.args.ips is not None) or (self.args.teams == "all") or (self.args.teams is not None and team['Id'] in self.args.teams):
