ad0a06f7f3eec637d658df5f59fa44d8ec99d4c3
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirbykohlmorgen@gmail.com>
2017-03-13 14:06:32 -0400

    Update mongo-messenger connection URL

--- security_lab/mongo-messenger/lib/messages.js
+++ security_lab/mongo-messenger/lib/messages.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
-const url = 'mongodb://localhost:27017/security-lab'
+const url = 'mongodb://node1:27001/security-lab?replicaSet=APPDB'
 let messages = null;
 
 MongoClient.connect(url, (err, db) => {
